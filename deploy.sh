aadf97d68f951a4f94680342db17aacd3c85a70b
c0llision <0xmw@protonmail.com>
2018-02-27 13:33:40 +0000

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+mkdir ~/.dotfiles.old
+
+mv ~/.aliases ~/.dotfiles.old/
+cp .aliases ~/
+
+mv ~/.gdbinit ~/.dotfiles.old/
+cp .gdbinit ~/
+
+mv ~/.gitconfig ~/.dotfiles.old/
+cp .gitconfig ~/
+
+mv ~/.inputrc ~/.dotfiles.old/
+cp .inputrc ~/
+
+mv ~/.profile ~/.dotfiles.old/
+cp .profile ~/
+
+mv ~/.vimrc ~/.dotfiles.old/
+cp .vimrc ~/
+
+mv ~/.ssh/config ~/.dotfiles.old/.ssh-config
+cp .ssh/config ~/.ssh/config
