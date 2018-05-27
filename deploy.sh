d3fc80b9f13461c8c92f0f60274263272c3c1a3f
c0llision <c0llision@protonmail.com>
2018-05-27 13:47:03 +0100

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,28 +6,25 @@ mv ~/.aliases ~/.dotfiles.old/
 cp aliases ~/.aliases
 
 mv ~/.curlrc ~/.dotfiles.old/
-cp curlrc ~/.curlrc 
+cp curlrc ~/.curlrc
 
 mv ~/.gdbinit ~/.dotfiles.old/
-cp gdbinit ~/.gdbinit 
+cp gdbinit ~/.gdbinit
 
 mv ~/.gitconfig ~/.dotfiles.old/
-cp gitconfig ~/.gitconfig 
+cp gitconfig ~/.gitconfig
 
 mv ~/.inputrc ~/.dotfiles.old/
-cp inputrc ~/.inputrc 
+cp inputrc ~/.inputrc
 
 mv ~/.profile ~/.dotfiles.old/
-cp profile ~/.profile 
+cp profile ~/.profile
 
 mv ~/.vimrc ~/.dotfiles.old/
-cp vimrc ~/.vimrc 
+cp vimrc ~/.vimrc
 
 mv ~/.ssh/config ~/.dotfiles.old/.ssh_config
 cp ssh_config ~/.ssh/config
 
 mv ~/.ssh/config_tor ~/.dotfiles.old/.ssh_config_tor
 cp ssh_config_tor ~/.ssh/config_tor
-
-rm -rf ~/.vim/bundle/Vundle.vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
